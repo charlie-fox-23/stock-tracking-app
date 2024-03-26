05758e630e007e1f32e26271088a66d192d70bb2
charlie-fox-23 <charliefox931@gmail.com>
2024-03-26 12:56:57 -0600

    completed display_investments function

--- stockTracker.py
+++ stockTracker.py
@@ -36,24 +36,27 @@ def save_investment (investment) :
     investments.append(investment)
     with investment_file.open("w") as file :
         return json.dump(investments,file)
-        
-
-
-
-
-
-
-
+    
 
 def display_investments() :
     investments = load_investments()
     for investment in investments   :
         symbol = investment["symbol"]
-        amount_invested =
-
-
-
-
+        amount_invested = investment["amount_invested"]
+        purchase_price = investment["purchase_price"]
+        shares_purchased = calculate_shares(amount_invested,purchase_price)
+        current_price = fetch_stock_price(symbol)
+        current_value = get_curent_value(shares_purchased,current_price)
+        profit_or_loss = get_profit_or_loss(amount_invested,current_value)
+        
+        print(f"Stock symbol:{symbol}")
+        print(f"Amount invested:{amount_invested}")
+        print(f"Purchase price:{purchase_price}")
+        print(f"Current price:{current_price}")
+        print(f"Current value:{current_value}")
+        print(f"Profit or loss:{profit_or_loss}")
+        print("-"*30)
+        
 
 
 
